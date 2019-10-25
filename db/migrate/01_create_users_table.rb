a804067f3b54b17e645c35c60d717ab916240596
Patrick Harrison <jahman07104@gmail.com>
2019-10-25 19:57:42 -0400

    Done.

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -1,11 +1,10 @@
-class CreateUsers < ActiveRecord::Migration
+
+class CreateUsersTable < ActiveRecord::Migration[4.2]
   def change
-    create_table :Users do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password
- 
-      t.timestamps
     end
   end
 end
